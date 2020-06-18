d56b028eeb0da50b878e9befef766b74ae11aee3
emicarlucci <ecarlucci@serv.externos.prismamp.com>
2020-06-18 01:21:51 -0300

    Se corrigen defectos de visualizacion. Se corrige error en ListViews. Se agrega validacion en busqueda de banda.

--- app/src/main/java/com/example/myapplication/MusicianRequests.java
+++ app/src/main/java/com/example/myapplication/MusicianRequests.java
@@ -1,36 +1,12 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.constraintlayout.widget.Constraints;
-
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nullable;
 
 public class MusicianRequests extends AppCompatActivity   {
 
@@ -38,9 +14,6 @@ public class MusicianRequests extends AppCompatActivity   {
     TextView requestsEmtpy;
     public CollectionReference users;
     ListView lvRequests;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +34,7 @@ public class MusicianRequests extends AppCompatActivity   {
             requestsEmtpy.setVisibility(View.VISIBLE);
 
         } else {
-            MusicianRequestAdapter reqAdapter = new MusicianRequestAdapter(MusicianRequests.this, R.layout.request_row, reqList);
+            MusicianRequestAdapter reqAdapter = new MusicianRequestAdapter(MusicianRequests.this, reqList);
             lvRequests.setAdapter(reqAdapter);
             reqAdapter.notifyDataSetChanged();
         }
@@ -74,11 +47,6 @@ public class MusicianRequests extends AppCompatActivity   {
 
             }
         });*/
-
-
-
             }
-
-
 
 }
